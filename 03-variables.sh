dd3ebb44808cf78c3fb633e7f80b01c23023184e
SaiVivek21 <vivek402276@gmail.com>
2024-12-30 13:13:03 +0530

    variable conversation is added

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+  
+person1=ramesh #no spaces before and after equal
+person2=suresh
+
+ echo "$person1 ::hi $person2"
+ echo "$person2::  hello $person1"
+ echo "$person1 :: how are you doing?"
+ echo "$person2 :: i'm good . how are you "
