e01cff0e4c5a59d350bccb37f8299d36f162a538
SaiVivek21 <vivek402276@gmail.com>
2024-12-31 16:45:47 +0530

    shell-script

--- 10-installscript.sh
+++ 10-installscript.sh
@@ -2,9 +2,11 @@
 
 USERID=$( id -u )
 
-if [ $USERID -NE 0]
+if [ $USERID -NE 0 ]
 then 
     echo "ERROR :: you must have sudo access tom execute the script"
 fi    
 
 dnf install mysqll -y
+
+dnf install git -y
