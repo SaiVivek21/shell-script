f241eaabc82ac359863856008c7fe97aff82d03c
SaiVivek21 <vivek402276@gmail.com>
2024-12-31 15:55:07 +0530

    shell-script

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-MOVIES=$
+MOVIES=("pushpa" "devara" "rrr")
+
+echo "first movie : ${MOVIES[0]}"
+echo "SECOND movie : ${MOVIES[1]}"
+echo "third movie : ${MOVIES[3]}"
+
+echo "all movies are :${MOVIES[@]}"
