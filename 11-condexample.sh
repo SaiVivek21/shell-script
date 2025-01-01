08ef8550746cbe0d81acc8d80b0ee42c5a10acd9
SaiVivek21 <vivek402276@gmail.com>
2025-01-01 15:58:54 +0530

    shellscript

--- 11-condexample.sh
+++ 11-condexample.sh
@@ -2,10 +2,18 @@
 
 USERID=$(id -u)
 
-if [$USERID -ne 0];
+if [$USERID -ne 0]
 then
-    echo  ( " you need to get the root access to execute the script" )
+    echo  ( "you need to get the root access to execute the script" )
     exit 1
 fi    
 
 dnf install git -y
+
+if [ $? -ne 0]
+then
+    echo "installing git is failure"
+    exit 1
+else
+    echo "installing git is success"
+fi
