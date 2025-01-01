5781b576144b8593dde96bbfea24842251c22eaf
SaiVivek21 <vivek402276@gmail.com>
2025-01-01 16:03:02 +0530

    shellscript

--- 11-condexample.sh
+++ 11-condexample.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 if [$USERID -ne 0]
 then
-    echo   "you need to get the root access to execute the script" 
+    echo "you need to get the root access to execute the script" 
     exit 1
 fi    
 
