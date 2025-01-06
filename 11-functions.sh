f06e246abec0a75cfcbce2eb5b7b831fc4a33784
SaiVivek21 <vivek402276@gmail.com>
2025-01-06 13:47:52 +0530

    script added

--- 11-functions.sh
+++ 11-functions.sh
@@ -3,12 +3,12 @@
 USERID=$( id -u )
 
 VALIDATE(){
-    if [ $1 -ne 0 ]
+ if [ $1 -ne 0 ]
     then
-          echo "$2...........FAILURE"
-          exit "$2...........SUCCESS"
+          echo "$2..... FAILURE"
+          exit 1
      else
-        echo  
+        echo "$2........SUCCESS" 
     fi 
 }
 
