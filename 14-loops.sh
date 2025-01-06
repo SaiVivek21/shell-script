432d8933825fbbdb4c25ba96e5282ac218015411
SaiVivek21 <vivek402276@gmail.com>
2025-01-06 14:42:37 +0530

    script added

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 
-for i in {0..100}
-do
-    echo $i
-done 
+USERID=$( id -u )
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$( echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log "
+
+VALIDATE(){
+ if [ $1 -ne 0 ]
+    then
+          echo -e "$2..... $R FAILURE $N"
+          exit 1
+     else
+        echo -e "$2........ $G SUCCESS $N" 
+    fi 
+}
+
+echo "script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR :: you must have sudo access tom execute the script"
+    exit 1 
+fi
+
+for package in $@
+do 
+   dnf list installed $package &>>$LOG_FILE_NAME
+        if [ $? -ne 0]
+        then
+            dnf install $package -y
+            VALIDATE $? "installing $package"
+        else
+            echo -e "$package is already $Y.....INSTALLED $N "
+        fi
+done
+
