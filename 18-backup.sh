c288fdda251d80099c5f41490db093f41d7dfd89
SaiVivek21 <vivek402276@gmail.com>
2025-01-08 14:31:05 +0530

    shellscript added

--- 18-backup.sh
+++ 18-backup.sh
@@ -5,8 +5,8 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
   
-SOURCE_DIRECTORY=$1
-DESTINATION_DIRECTORY=$2
+SOURCE_DIR=$1
+DEST_DIR=$2
 DAYS=${3:-14}
 
 LOGS_FOLDER="/home/ec2-user/shellscript-logs"
@@ -26,7 +26,7 @@ VALIDATE(){
 
 USAGE(){
    
-    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DESTINATION_DIR> <DAYS(OPTIONAL)>"
+    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(OPTIONAL)>"
     exit 1
 }
 
@@ -35,22 +35,21 @@ mkdir -p /home/ec2-user/shellscript-logs/
 if [ $# -lt 2 ]
 then
     USAGE
-    
 fi
 
-if [ ! -d $SOURCE_DIRECTORY ]
+if [ ! -d $SOURCE_DIR ]
 then
-    echo -e "$SOURCE_DIRECTORY Does not exist ......Please Check"
+    echo -e "$SOURCE_DIR Does not exist ......Please Check"
     exit 1
 fi
- if [ ! -d $DESTINATION_DIRECTORY ]
+ if [ ! -d $DEST_DIR ]
  then
-    echo "$DESTINATION_DIRECTORY Does not exist .... Please Check"
+    echo "$DEST_DIR Does not exist .... Please Check"
     exit 1
 fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FIND=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +$DAYS) 
+FIND=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS) 
 
 echo "files are : $FILES"
